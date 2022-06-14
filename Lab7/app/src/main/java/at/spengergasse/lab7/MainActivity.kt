be621cb5d0f4ace3f5ae075235769c72a6f24438
Zander187 <96262138+Zander187@users.noreply.github.com>
2022-06-14 22:53:01 +0200

    1. Teil programmiert
    Variablen deklariert, GUI Module ins Mainactivity.kt eingebunden, Oncklicklistener erstellt

--- Lab7/app/src/main/java/at/spengergasse/lab7/MainActivity.kt
+++ Lab7/app/src/main/java/at/spengergasse/lab7/MainActivity.kt
@@ -4,10 +4,25 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
+    private var btn: Button? = null
+    var result = 0.0
+    private var a: EditText? = null
+    private var b: EditText? = null
+    private var Ergebnis: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btn = findViewById(R.id.btn)
+        a = findViewById(R.id.a)
+        b = findViewById(R.id.b)
+        Ergebnis = findViewById(R.id.Ergebnis)
+
+
+
+        with(btn) { this?.setOnClickListener(View.OnClickListener { berrechene() }) }
+
     }
 
 }
